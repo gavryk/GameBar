99f37962b9e482c3669962d6441842a876c66a2c
Gavryk <gvozdyk23@gmail.com>
2022-05-27 03:06:39 +0300

    some api settings

---
+++ src/api/api.js
@@ -0,0 +1,25 @@
+//Base URL
+const api_url = "https://api.rawg.io/api/";
+
+//Getting the date
+const getCurrentDay = () => {
+    let day = new Date().getDate();
+    return day < 10 ? `0${day}` : day;
+}
+const getCurrentMonth = () => {
+    let month = new Date().getMonth() + 1;
+    return month < 10 ? `0${month}` : month;
+}
+
+//Current year-month-day
+const currentYear = new Date().getFullYear(),
+      currentMonth = getCurrentMonth(),
+      currentDay = getCurrentDay(),
+      currentDate = `${currentYear}-${currentMonth}-${currentDay}`,
+      lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`,
+      nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${api_url}${popular_games}`;
