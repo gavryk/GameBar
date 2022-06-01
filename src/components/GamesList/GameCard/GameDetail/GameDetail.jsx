0dc68dc34e3917d64c37a155ba1ada1e52e65b85
Gavryk <gvozdyk23@gmail.com>
2022-06-02 01:41:05 +0300

    some styles for popup

--- src/components/GamesList/GameCard/GameDetail/GameDetail.jsx
+++ src/components/GamesList/GameCard/GameDetail/GameDetail.jsx
@@ -3,9 +3,12 @@ import style from "./GameDetail.module.scss";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { smallImage } from "../../../../utils/utils";
 
 const GameDetail = () => {
-  const { game, screenshots, isLoading } = useSelector((state) => state.gameDetail);
+  const { game, screenshots, isLoading } = useSelector(
+    (state) => state.gameDetail
+  );
   const navigate = useNavigate();
 
   //Exit Detail
@@ -13,38 +16,40 @@ const GameDetail = () => {
     const el = e.target;
     document.body.style.overflow = "auto";
     navigate("/", { replace: true });
-  }
+  };
 
   return (
     <>
       {!isLoading && (
         <motion.div className={style.gameDetail} onClick={exitDetailHandler}>
-          <div className={style.detail}>
-            <div className="stats">
-              <div className="rating">
-                <h3>{game.name}</h3>
-                <p>Rating: {game.rating}</p>
-              </div>
-              <div className="info">
-                <h3>Platforms</h3>
-                <div className="platforms">
-                  {game.platforms.map(({ platform }) => {
-                    return <h4 key={platform.id}>{platform.name}</h4>;
-                  })}
+          <div className={style.detailWrapper}>
+            <div className={style.detail}>
+              <div className={style.stats}>
+                <div className="rating">
+                  <h3>{game.name}</h3>
+                  <p>Rating: {game.rating}</p>
+                </div>
+                <div className={style.info}>
+                  <h3>Platforms</h3>
+                  <div className={style.platforms}>
+                    {game.platforms.map(({ platform }) => {
+                      return <h4 key={platform.id}>{platform.name}</h4>;
+                    })}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="media">
-              <img src={game.background_image} alt={game.name} />
-            </div>
-            <div className="description">
-              <p>{game.description_raw}</p>
-            </div>
-            <div className="gallery">
-              {screenshots &&
-                screenshots.map(({ image, id }) => {
-                  return <img key={id} src={image} alt={game.name} />;
-                })}
+              <div className="media">
+                <img src={smallImage(game.background_image, 1280)} alt={game.name} />
+              </div>
+              <div className="description">
+                <p>{game.description_raw}</p>
+              </div>
+              <div className="gallery">
+                {screenshots &&
+                  screenshots.map(({ image, id }) => {
+                    return <img key={id} src={smallImage(image, 1280)} alt={game.name} />;
+                  })}
+              </div>
             </div>
           </div>
         </motion.div>
